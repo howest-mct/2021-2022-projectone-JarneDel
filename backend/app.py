ff23772dd269afc3a4926e67a54c8f762925cf1c
JarneDel <jarne.delarue@student.howest.be>
2022-05-28 13:39:04 +0200

    feat: fijnstofsensor uitlezen

--- backend/app.py
+++ backend/app.py
@@ -10,128 +10,145 @@ from repositories.DataRepository import DataRepository
 
 from selenium import webdriver
 
+from serial import Serial, PARITY_NONE
+from model.pms5003 import Pms5003
+
 # from selenium import webdriver
 # from selenium.webdriver.chrome.options import Options
 
-
-ledPin = 21
-btnPin = Button(20)
 
 # Code voor Hardware
 def setup_gpio():
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
 
-    GPIO.setup(ledPin, GPIO.OUT)
-    GPIO.output(ledPin, GPIO.LOW)
-    
-    btnPin.on_press(lees_knop)
-
-
-def lees_knop(pin):
-    if btnPin.pressed:
-        print("**** button pressed ****")
-        if GPIO.input(ledPin) == 1:
-            switch_light({'lamp_id': '3', 'new_status': 0})
-        else:
-            switch_light({'lamp_id': '3', 'new_status': 1})
-
-
-
 
 # Code voor Flask
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'geheim!'
-socketio = SocketIO(app, cors_allowed_origins="*", logger=False,
-                    engineio_logger=False, ping_timeout=1)
+app.config["SECRET_KEY"] = "3fan6od4@PG$BfGH"
+socketio = SocketIO(
+    app, cors_allowed_origins="*", logger=False, engineio_logger=False, ping_timeout=1
+)
 
 CORS(app)
 
 
-@socketio.on_error()        # Handles the default namespace
+@socketio.on_error()  # Handles the default namespace
 def error_handler(e):
     print(e)
 
+
+# pms init
+pms = Pms5003(set=20, reset=21)
+pms.setup()
+
+# region Main Thread
+def main_thread():
+
+    # kan nog niet doen, weet nog niet hoe 2de uart bus werkt
+    while True:
+        # val = read_mhz19b()
+        # DataRepository.add_data_point(val, 1, 2)
+        # socketio.emit("B2F_CO2", {"CO2": val})
+        time.sleep(1)
+        list_data, dict_data = pms.read()  # dict met 9 data punten
+        # voorlopig hardcoded
+        for i, datapunt in enumerate(list_data):
+            # pm sensor begint bij eenheidID2
+            DataRepository.add_data_point(datapunt, 1, 3 + i)
+
+        socketio.emit("B2F_PM", dict_data)
+        time.sleep(60)
+
+
+# endregion
+
+# Region SENSORS
+def read_mhz19b():
+    data = serial_send_and_receive(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
+    val = int.from_bytes(data[2:4], "big")
+    return val
+
+
+def calibrate_mhz10b():
+    # zero point calibration
+    data = serial_send_and_receive(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
+    val = int.from_bytes(data[2:4], "big")
+    if val < 420:
+        serial_send_and_receive(b"\xff\x01\x87\x00\x00\x00\x00\x00")
+        print("calibrated")
+
+
+def serial_send_and_receive(msg):
+
+    with Serial(
+        "/dev/ttyS0",
+        9600,
+        bytesize=8,
+        parity=PARITY_NONE,
+        stopbits=1,
+        timeout=3,
+    ) as port:
+        try:
+            port.write(msg)
+            return port.read(9)
+
+        except Exception as ex:
+            print(f"Er is een fout opgetreden: {ex}")
+            return " "
+
+
+# endregion
 
 
 # API ENDPOINTS
 
 
-@app.route('/')
+@app.route("/")
 def hallo():
     return "Server is running, er zijn momenteel geen API endpoints beschikbaar."
 
 
-@socketio.on('connect')
+@socketio.on("connect")
 def initial_connection():
-    print('A new client connect')
+    print("A new client connect")
+    data = DataRepository.get_all_recent_data()
+    emit("B2F_Actuele_data", data)
     # # Send to the client!
     # vraag de status op van de lampen uit de DB
-    status = DataRepository.read_status_lampen()
-    emit('B2F_status_lampen', {'lampen': status}, broadcast=True)
 
-
-@socketio.on('F2B_switch_light')
-def switch_light(data):
-    # Ophalen van de data
-    lamp_id = data['lamp_id']
-    new_status = data['new_status']
-    print(f"Lamp {lamp_id} wordt geswitcht naar {new_status}")
-
-    # Stel de status in op de DB
-    res = DataRepository.update_status_lamp(lamp_id, new_status)
-
-    # Vraag de (nieuwe) status op van de lamp en stuur deze naar de frontend.
-    data = DataRepository.read_status_lamp_by_id(lamp_id)
-    socketio.emit('B2F_verandering_lamp', {'lamp': data}, broadcast=True)
-
-    # Indien het om de lamp van de TV kamer gaat, dan moeten we ook de hardware aansturen.
-    if lamp_id == '3':
-        print(f"TV kamer moet switchen naar {new_status} !")
-        GPIO.output(ledPin, new_status)
-
-
-
-# START een thread op. Belangrijk!!! Debugging moet UIT staan op start van de server, anders start de thread dubbel op
-# werk enkel met de packages gevent en gevent-websocket.
-def all_out():
-    while True:
-        print('*** We zetten alles uit **')
-        DataRepository.update_status_alle_lampen(0)
-        GPIO.output(ledPin, 0)
-        status = DataRepository.read_status_lampen()
-        socketio.emit('B2F_status_lampen', {'lampen': status})
-        time.sleep(15)
 
 def start_thread():
     print("**** Starting THREAD ****")
-    thread = threading.Thread(target=all_out, args=(), daemon=True)
+    thread = threading.Thread(target=main_thread, args=(), daemon=True)
     thread.start()
 
 
 def start_chrome_kiosk():
     import os
 
-    os.environ['DISPLAY'] = ':0.0'
+    os.environ["DISPLAY"] = ":0.0"
     options = webdriver.ChromeOptions()
     # options.headless = True
     # options.add_argument("--window-size=1920,1080")
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36")
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--allow-running-insecure-content')
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36"
+    )
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--allow-running-insecure-content")
     options.add_argument("--disable-extensions")
     # options.add_argument("--proxy-server='direct://'")
     options.add_argument("--proxy-bypass-list=*")
     options.add_argument("--start-maximized")
-    options.add_argument('--disable-gpu')
+    options.add_argument("--disable-gpu")
     # options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--kiosk')
-    # chrome_options.add_argument('--no-sandbox')         
+    options.add_argument("--no-sandbox")
+    options.add_argument("--kiosk")
+    # chrome_options.add_argument('--no-sandbox')
     # options.add_argument("disable-infobars")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
+    options.add_experimental_option("useAutomationExtension", False)
 
     driver = webdriver.Chrome(options=options)
     driver.get("http://localhost")
@@ -145,19 +162,18 @@ def start_chrome_thread():
     chromeThread.start()
 
 
-
 # ANDERE FUNCTIES
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         setup_gpio()
         start_thread()
         start_chrome_thread()
         print("**** Starting APP ****")
-        socketio.run(app, debug=False, host='0.0.0.0')
+        socketio.run(app, debug=False, host="0.0.0.0")
     except KeyboardInterrupt:
-        print ('KeyboardInterrupt exception is caught')
+        print("KeyboardInterrupt exception is caught")
     finally:
+        pms.cleanup()
         GPIO.cleanup()
-
