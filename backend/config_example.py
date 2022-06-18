0a2d0ecbf32148dff8fd98fc7f3ed2e159215cd0
Delarue Jarne <91070860+JarneDel@users.noreply.github.com>
2022-06-18 16:27:02 +0200

    fix: removed unnececary files

---
+++ backend/config_example.py
@@ -0,0 +1,9 @@
+[connector_python]
+user = username
+host = 127.0.0.1
+port = 3306
+password = <insert password here>
+database = AirInsightDB
+
+[application_config]
+driver = 'SQL Server'
--- backend/test_fan.py
+++
@@ -1,9 +0,0 @@
-import imp
-from model.Fan import Fan
-from time import sleep
-
-fan = Fan(6, 13, 2, initial=100)
-
-while True:
-    print(fan.rpm)
-    sleep(0.2)
